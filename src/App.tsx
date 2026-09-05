5c05213f0651b8ba34aec5e033bb88827b7d363f
Bilal Malik <bilalmlkdev@gmail.com>
2026-09-05 12:11:45 +0500

    fix: add ScrollToTop component to reset scroll position on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
-import NavBar from './components/NavBar'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import ImageGenerator from './pages/ImageGenerator'
-import TextGenerator from './pages/TextGenerator'
-import EmojiGenerator from './pages/EmojiGenerator'
-import SvgGenerator from './pages/SvgGenerator'
-import NotFound from './pages/NotFound'
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
+import Home from "./pages/Home";
+import ImageGenerator from "./pages/ImageGenerator";
+import TextGenerator from "./pages/TextGenerator";
+import EmojiGenerator from "./pages/EmojiGenerator";
+import SvgGenerator from "./pages/SvgGenerator";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
     <div className="flex min-h-screen flex-col bg-ink">
+      <ScrollToTop />
       <NavBar />
       <main className="flex-1 animate-fade-in">
         <Routes>
@@ -24,5 +26,5 @@ export default function App() {
       </main>
       <Footer />
     </div>
-  )
+  );
 }
